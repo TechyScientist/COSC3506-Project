445dfb4d9ccfcfc71c5fedf31aad50d70a89e007
Johnny Console <johnny.console@hotmail.com>
2020-01-21 14:00:43 -0500

    Add Database Server Details

--- src/cosc3506/project/Database.java
+++ src/cosc3506/project/Database.java
@@ -12,11 +12,20 @@ import java.sql.SQLException;
  * Written: 16 Jan 2020
  */
 public class Database {
-    public static final String HOST = "";
+    public static final String HOST = "10.14.3.46";
+    //The above host is only available on AlgomaU networked computers.
     public static final String DB = "project";
     public static final String USER = "dev";
-    public static final String PASS = "project";
+    public static final String PASS = "ProjectDev#2020";
 
+    /**
+     * Connects to a database
+     * @param host The IP Address or Hostname of the Database Server
+     * @param user The username to connect as
+     * @param pass The password for the connectiong user
+     * @param db The name of the database to use
+     * @return A connection to the database
+     */
     public static Connection connect(String host, String user, String pass, String db) {
         Connection conn = null;
         try {
