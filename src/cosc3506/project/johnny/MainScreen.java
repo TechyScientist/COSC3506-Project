7688d915cbdc038f07a2291fdd24d8c85453f41d
techyscientist <Marbles#1999>
2020-02-12 09:50:33 -0500

    Implement add user feature

--- src/cosc3506/project/johnny/MainScreen.java
+++ src/cosc3506/project/johnny/MainScreen.java
@@ -4,13 +4,9 @@ import javafx.application.Application;
 import javafx.geometry.*;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.input.KeyCode;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
-
-import static javafx.scene.text.FontWeight.BOLD;
-import static javafx.scene.text.FontPosture.ITALIC;
 
 public class MainScreen extends Application {
 
@@ -35,23 +31,51 @@ public class MainScreen extends Application {
         pane.setHgap(10);
 
         Label title = new Label("Welcome,  " + (!name.contains(" ")? name : name.substring(0, name.indexOf(" "))) + "!");
+        GridPane.setHalignment(title, HPos.CENTER);
         title.setFont(Font.font(title.getFont().getFamily(), 16));
         pane.add(title, 0, 0, 3, 1);
 
-        Button directory = new Button("Directory");
-        directory.setFont(Font.font(directory.getFont().getFamily(), 12));
-        directory.setMaxWidth(Double.MAX_VALUE);
-        pane.add(directory, 1, row);
-
-        Button inbox = new Button("Inbox");
+        Button inbox = new Button("Inbox (I)");
         inbox.setFont(Font.font(inbox.getFont().getFamily(), 12));
         inbox.setMaxWidth(Double.MAX_VALUE);
         pane.add(inbox, 0, row);
 
-        Button logout = new Button("Log Out");
+        Button directory = new Button("Directory (D)");
+        directory.setFont(Font.font(directory.getFont().getFamily(), 12));
+        directory.setMaxWidth(Double.MAX_VALUE);
+        pane.add(directory, 1, row);
+
+        Button profile = new Button("Your Profile (P)");
+        profile.setFont(Font.font(profile.getFont().getFamily(), 12));
+        profile.setMaxWidth(Double.MAX_VALUE);
+        pane.add(profile, 2, row);
+
+        Button logout = new Button("Log Out (Esc)");
         logout.setFont(Font.font(logout.getFont().getFamily(), 12));
         logout.setMaxWidth(Double.MAX_VALUE);
         pane.add(logout, 0, ++row, 3, 1);
+
+        if(!MainScreen.type.equalsIgnoreCase("standard")) {
+            Button addU = new Button("Add User (A)");
+            Button delU = new Button("Delete User (R)");
+            Button chUPass = new Button("Reset Password (X)");
+
+            addU.setFont(Font.font(addU.getFont().getFamily(), 12));
+            addU.setMaxWidth(Double.MAX_VALUE);
+            pane.add(addU, 0, ++row, 3, 1);
+            addU.setOnAction(e -> {
+                ps.close();
+                new UserAdd().start(new Stage());
+            });
+
+            delU.setFont(Font.font(delU.getFont().getFamily(), 12));
+            delU.setMaxWidth(Double.MAX_VALUE);
+            pane.add(delU, 0, ++row, 3, 1);
+
+            chUPass.setFont(Font.font(chUPass.getFont().getFamily(), 12));
+            chUPass.setMaxWidth(Double.MAX_VALUE);
+            pane.add(chUPass, 0, ++row, 3, 1);
+        }
 
         directory.setOnAction(e -> {
             ps.close();
@@ -66,11 +90,36 @@ public class MainScreen extends Application {
 
 
         pane.setOnKeyPressed(k -> {
-            if (k.getCode() == KeyCode.ESCAPE) {
-                name = user = type = null;
-                ps.close();
-                new LoginScreen().start(new Stage());
+            switch(k.getCode()) {
+                case ESCAPE:
+                    name = user = type = null;
+                    ps.close();
+                    new LoginScreen().start(new Stage());
+                    break;
+                case I:
+                    break;
+                case D:
+                    ps.close();
+                    new Directory().start(new Stage());
+                    break;
+                case P:
+                    break;
+                case A:
+                    if(!MainScreen.type.equalsIgnoreCase("standard")) {
+                        ps.close();
+                        new UserAdd().start(new Stage());
+                    }
+                    break;
+                case R:
+                    if(!MainScreen.type.equalsIgnoreCase("standard")) {
 
+                    }
+                    break;
+                case X:
+                    if(!MainScreen.type.equalsIgnoreCase("standard")) {
+
+                    }
+                    break;
             }
         });
 
@@ -80,7 +129,7 @@ public class MainScreen extends Application {
             new LoginScreen().start(new Stage());
         });
 
-        Scene scene = new Scene(pane,550, 400);
+        Scene scene = new Scene(pane,300, (!MainScreen.type.equalsIgnoreCase("standard") ? 400 : 200));
         ps.setScene(scene);
         ps.setTitle("Session: " + user);
         ps.show();
